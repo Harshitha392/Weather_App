21d8d573c2a298637c66bec41d0ab3322a36e262
Harshitha392 <harshitha.majety@gmail.com>
2023-02-16 17:54:40 +0530

    added html file

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 5010;
 app.get("/", function(req, res){
 	res.send("Hello World");
 });
-
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
 	console.log(`Server running on http://localhost:${port}`);
