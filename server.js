27781d90741dc4dd7b02306fbf983a5d0e8ad039
Harshitha392 <harshitha.majety@gmail.com>
2023-02-16 18:04:14 +0530

    updated

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get("/resume", function(req, res){
 	//res.send("I am Harshitha Majety");
 	res.sendFile(__dirname+"/resume.html");
 });
+app.get("/resume1",function(req,res){
+    res.sendFile(__dirname+"/resume1.html");
+});
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
 	console.log(`Server running on http://localhost:${port}`);
