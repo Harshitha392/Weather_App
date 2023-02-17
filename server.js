71959a64916f98d55f87eb3d92f2b5402a0fb093
Harshitha392 <harshitha.majety@gmail.com>
2023-02-17 05:50:04 +0530

    added card.html

--- server.js
+++ server.js
@@ -33,6 +33,12 @@ app.get("/resume", function(req, res){
 	//res.send("I am Harshitha Majety");
 	res.sendFile(__dirname+"/resume.html");
 });
+
+app.get("/card", function(req, res){
+	//res.send("I am Harshitha Majety");
+	res.sendFile(__dirname+"/card.html");
+});
+
 mongoose.set('strictQuery',true);
 mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 	if(err)
@@ -40,17 +46,17 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 	else{
 	console.log("db connected");
 	//do not create user if atleast 1 user exist in table
-	userLib.createFirstUser(function(err,res){
-		if(err){
-		console.error(err);
-		}
-		else
-		console.log(res);
-	});
-	app.listen(port, function(){
-		console.log("Server running on http://localhost:"+port);
-		console.log(`Server running on http://localhost:${port}`);
-	});
+	// userLib.createFirstUser(function(err,res){
+	// 	if(err){
+	// 	console.error(err);
+	// 	}
+	// 	else
+	// 	console.log(res);
+	// });
+	// app.listen(port, function(){
+	// 	console.log("Server running on http://localhost:"+port);
+	// 	console.log(`Server running on http://localhost:${port}`);
+	// });
 
         //get user by name
         // userLib.getUserByUserName("harshitha12", function(err, res) {
