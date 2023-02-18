47ab55d4579cc38d42446687ac621e3c30e11228
Harshitha392 <harshitha.majety@gmail.com>
2023-02-18 09:51:11 +0530

    added weather html

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 5010;
-
-// app.get("/", function(req, res){
-// 	//res.send("I am Harshitha Majety");
-// 	res.sendFile(__dirname+"/index.html");
-// });
-
-// app.get("/resume", function(req, res){
-// 	//res.send("I am Harshitha Majety");
-// 	res.sendFile(__dirname+"/resume.html");
-// });
-// app.get("/resume1",function(req,res){
-//     res.sendFile(__dirname+"/resume1.html");
-// });
-// app.listen(port, function(){
-// 	console.log("Server running on http://localhost:"+port);
-// 	console.log(`Server running on http://localhost:${port}`);
-// });
 require("dotenv").config();
 const userLib=require("./backend/Lib/userLib")
 const mongoose =require("mongoose");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
+
+const options = {
+	extensions:['htm','html','css','js','ico','jpg','jpeg','png','svg','pdf'],
+	index:['index.html'],
+}
+app.use(express.static("public",options));
 
 app.get("/", function(req, res){
 	//res.send("I am Harshitha Majety");
@@ -39,6 +25,11 @@ app.get("/card", function(req, res){
 	res.sendFile(__dirname+"/card.html");
 });
 
+app.get("/weather", function(req, res){
+	//res.send("I am Harshitha Majety");
+	res.sendFile(__dirname+"/weather.html");
+});
+
 mongoose.set('strictQuery',true);
 mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 	if(err)
@@ -46,13 +37,13 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 	else{
 	console.log("db connected");
 	//do not create user if atleast 1 user exist in table
-	userLib.createFirstUser(function(err,res){
-		if(err){
-		console.error(err);
-		}
-		else
-		console.log(res);
-	});
+	// userLib.createFirstUser(function(err,res){
+	// 	if(err){
+	// 	console.error(err);
+	// 	}
+	// 	else
+	// 	console.log(res);
+	// });
 	app.listen(port, function(){
 		console.log("Server running on http://localhost:"+port);
 		console.log(`Server running on http://localhost:${port}`);
